889d8c9991d92873ffa14ea4cab39ab846ea22ce
mario camayo <mariocamayo17@gmail.com>
2022-08-23 18:19:23 -0500

    se agrego varios for

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ console.log('hola peru')
 for(let i =0;i<2;i++){
     return i
 }
+for(let i =0;i<2;i++){
+    return i
+}
+for(let i =0;i<2;i++){
+    return i
+}
+for(let i =0;i<2;i++){
+    return i
+}
+for(let i =0;i<2;i++){
+    return i
+}
